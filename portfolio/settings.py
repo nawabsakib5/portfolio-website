3cf08f9481f73dcc1b2859afd7d4a83e150f105d
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-03 03:21:59 +0600

    Add views, templates and connect Profile/Project models

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Email
@@ -129,5 +131,3 @@ MAILERS = {
 
 
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "portfolio_app" / "static"]

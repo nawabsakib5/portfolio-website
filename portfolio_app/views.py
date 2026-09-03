2305392f7b51d82c65aef6c8e5613d179da1d2fe
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-04 01:53:08 +0600

    Add Database

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from .models import*
 
+
+
 def home(request):
+    profile = Profile.objects.first()
+    projects = Project.objects.all()
+    skills = Skill.objects.all()
+    experiences = Experience.objects.all()
+    educations = Education.objects.all()
+    certificates = Certificate.objects.all()
+
     context = {
-        'profile': Profile.objects.first(),
-        'projects': Project.objects.all(),
-        'skills': Skill.objects.all(),
-        'experiences': Experience.objects.all(),
-        'educations': Education.objects.all(),
+        'profile': profile,
+        'projects': projects,
+        'skills': skills,
+        'experiences': experiences,
+        'educations': educations,
+        'certificates': certificates,
     }
     return render(request, 'portfolio_app/home.html', context)
 

f02e99e8e95f6fc6e48d557bde9cdc6e4b347c3a
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-05 00:18:11 +0600

    Complete portfolio website with Django backend

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import*
+from .models import *
 
 admin.site.register(Profile)
 admin.site.register(Project)
@@ -7,3 +7,13 @@ admin.site.register(Skill)
 admin.site.register(Experience)
 admin.site.register(Education)
 admin.site.register(Certificate)
+
+# --- NEW MODELS REGISTRATION ---
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    readonly_fields = ('name', 'email', 'subject', 'message', 'created_at')
+
+@admin.register(Analytics)
+class AnalyticsAdmin(admin.ModelAdmin):
+    list_display = ('projects_built', 'apis_designed', 'uptime_percentage', 'git_commits', 'cv_downloads')

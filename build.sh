6bfb303808ee7a2fd4e21352c475adf4a8ce0877
Mohammad Sakib Howlader <nawabsakib5@gmail.com>
2026-09-06 03:34:06 +0600

    Fix superuser creation format

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ python manage.py shell << 'EOF'
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'nawabsakib5@gmail.com', 'Admin@1234')
+    User.objects.create_superuser('admin', 'nawabsakib5@gmail.com', 'Admin@@##1234')
     print('Superuser created!')
 else:
     print('Superuser already exists.')
